1278c945618401e5cb2ef63e469dde82a357f356
neilgfoster <1370457+neilgfoster@users.noreply.github.com>
2025-09-15 21:27:28 +0100

    fix: correct echo command and improve git config retrieval in finalization script

--- .setup/scripts/999-finalise.sh
+++ .setup/scripts/999-finalise.sh
@@ -6,14 +6,15 @@ set -e
 # Echo messages to the user
 echo
 echo -e "${GREEN}Setup complete!${NC}"
+echo
 
 # Check if git username and email are set
-GIT_NAME=$(git config --global user.name)
-GIT_EMAIL=$(git config --global user.email)
+GIT_NAME=$(git config --global user.name || true)
+GIT_EMAIL=$(git config --global user.email || true)
 if [[ -z "$GIT_NAME" || -z "$GIT_EMAIL" ]]; then
   echo -e "${GREEN}Note that you may need to set your Git username and email if you are committing changes within this devcontainer.${NC}"
   echo -e "${GREEN}You can do this by running the following commands:${NC}"
   echo -e "${YELLOW}git config --global user.name 'Your Name'${NC}"
   echo -e "${YELLOW}git config --global user.email 'you@example.com'${NC}"
 fi
-echo
+echos
